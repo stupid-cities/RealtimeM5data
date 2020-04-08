b755beaa4dbfc278039da38af389a438c0edcb78
Dave Webb <dave.webb@cranbim.com>
2020-04-08 15:31:28 +0100

    New Sketch to interpret IMU data from firebase

---
+++ SC_P5JSclient/SurfaceDataViz1/sketch.js
@@ -0,0 +1,130 @@
+var ranges={};
+// var GYRX=0, GYRY=1, GYRZ=2, ACCX=3, ACCY=4, ACCZ=5, ROLL=6, PITC=7, YAWW=8;
+var types=["GYRX","GYRY","GYRZ","ACCX","ACCY","ACCZ","PITC","ROLL","YAWW","TIME"];
+
+// var gyroSensitivity=-20;
+// var accSensitivity=-500;
+// var gyroRange=-0.01;
+
+var surfaceData;
+var lastDataSet=null;
+var structured;
+var dataRetrieved=false;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  background(0,0,10);
+  // for(var i=0; i<4; i++){
+  //   graphs.push(new Graph(width*0.1,height*(0.1+0.18*i),width*0.7,height*0.15,10,100,"Noise data"+nf(i,2,0),20+i*30));
+  // }
+  var i=0;
+
+  // Initialize Firebase
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyCjRAbWqnr87M5l5p-femOmfhX8OCRPNLk",
+    authDomain: "stupidcitiessurface.firebaseapp.com",
+    databaseURL: "https://stupidcitiessurface.firebaseio.com",
+    projectId: "stupidcitiessurface",
+    storageBucket: "stupidcitiessurface.appspot.com",
+    messagingSenderId: "656281181425",
+    appId: "1:656281181425:web:980ebbfe2d733b7f1b9d50",
+    measurementId: "G-XEJS1H4QPD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+	surfaceData = firebase.database();
+	retrieveData();
+}
+
+function retrieveData(){
+  var ref=surfaceData.ref('surface');
+  ref.once('value', function(snapshot) {
+    var data=snapshot.val();
+    // console.log(data);
+    datakeys=Object.keys(data);
+    console.log(datakeys);
+    // data.forEach(function(
+    var lastDataIndex=-1;
+    var newest=0;
+    datakeys.forEach(function(dk,i){
+      var thisDate=data[dk].date;
+      console.log(data[dk].date+" "+new Date(data[dk].date));
+      if(thisDate>newest){
+        newest=thisDate;
+        lastDataIndex=i;
+      }
+    });
+    if(lastDataIndex>-1){
+      lastDataSet=data[datakeys[lastDataIndex]];
+      dataRetrieved=true;
+      structured=new StructuredIMUData(lastDataSet);
+    }
+  });
+  // noLoop();
+}
+
+
+
+function draw() {
+  background(0,0,10);
+  fill(300,100,100);
+  noStroke();
+  textSize(10);
+  textAlign(LEFT,BASELINE);
+  text(dataRetrieved,100,100);
+  if(dataRetrieved){
+    text(new Date(lastDataSet.date),100,120);
+    drawData(structured,100,100,400,250);
+    noLoop();
+  }
+}
+
+function drawData(data,x,y,w,h){
+  var timeSpan=data.TIME[data.TIME.length-1];
+  var xStep=w/timeSpan;
+  console.log(timeSpan,xStep);
+  push();
+  translate(x,y);
+  fill(0,0,40);
+  stroke(0,0,100);
+  rect(0,0,w,h);
+  translate(0,h);
+  var xProg=0;
+  line(0,100,100);
+  console.log(data.size);
+  for(var i=0; i<data.size; i++){
+    var ay=data.ACCY[i];
+    xProg=data.TIME[i]*xStep;
+    console.log(xProg, ay);
+    // ellipse(xProg,-1,2);
+    line(xProg,0,xProg,-ay*100);
+  }
+  pop();
+  // noLoop();
+}
+  
+
+function StructuredIMUData(rawData){
+  this.size=rawData.values.length;
+  this.date=rawData.date;
+  this.time=[];
+  var sid=this;
+  
+  rawData.values.forEach(function(row){
+    var cols=Object.keys(row);
+    cols.forEach(function(col){
+      if(!sid[col]){
+        sid[col]=[];
+      } 
+      sid[col].push(row[col]);
+    });
+  });
+  console.log(this);
+}
+    
+
+
